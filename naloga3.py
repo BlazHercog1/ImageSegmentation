75bda723379a38939c9cf64b9501c9a8ef0e1270
BlazHercog1 <blaz.hercog@gmail.com>
2025-04-28 11:51:20 +0200

    Update

--- naloga3.py
+++ naloga3.py
@@ -1,15 +1,15 @@
 import cv2 as cv
 
 def kmeans(slika, k=3, iteracije=10):
-    '''Izvede segmentacijo slike z uporabo metode k-means.'''
+    '''Izvede segmentacijo slike z uporabo metode k-means.1'''
     pass
 
 def meanshift(slika, velikost_okna, dimenzija):
-    '''Izvede segmentacijo slike z uporabo metode mean-shift.'''
+    '''Izvede segmentacijo slike z uporabo metode mean-shift.2'''
     pass
 
 def izracunaj_centre(slika, izbira, dimenzija_centra, T):
-    '''Izračuna centre za metodo kmeans.'''
+    '''Izračuna centre za metodo kmeans.3'''
     pass
 
 if __name__ == "__main__":
